c712c408115efcab6bd0e6e7ffb80ba4a1dd6ec1
Arne <1255879+arneee@users.noreply.github.com>
2021-10-30 15:35:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,3 +12,14 @@ if(isset($_GET['message']) && $_GET['message'] != '') {
 
 $cow = Farm::create(\Cowsayphp\Farm\Cow::class);
 echo $cow->say($text);
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('path/to/your.log', Logger::DEBUG));
+$log->pushHandler(new StreamHandler('php://stdout', Logger::DEBUG));
+
+// add records to the log
+$log->warning('hello',$_ENV);
+$log->error('you',$_SERVER);
+
+echo "\n\nOK!";
